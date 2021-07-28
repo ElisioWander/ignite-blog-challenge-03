15938c282d52e07ee68dc82e005b87da00252064
elisioWander <elisio741@hotmail.com>
2021-07-28 03:01:04 -0300

    page-index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
+import styles from './header.module.scss'
+
 export default function Header() {
-  
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/Logo.svg" alt="Logo" />
+      </div>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
+import Head from 'next/head';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,7 +26,44 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <h1>Test</h1>
+    <>
+      <Head>
+        <title>Home | EWW-blog</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <h2>Pensando em sincronização em vez de ciclos de vida</h2>
+            <time>15 mar 2021</time>
+            <span>Diego Fernandes</span>
+          </a>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <h2>Pensando em sincronização em vez de ciclos de vida</h2>
+            <time>15 mar 2021</time>
+            <span>Diego Fernandes</span>
+          </a>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <h2>Pensando em sincronização em vez de ciclos de vida</h2>
+            <time>15 mar 2021</time>
+            <span>Diego Fernandes</span>
+          </a>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <h2>Pensando em sincronização em vez de ciclos de vida</h2>
+            <time>15 mar 2021</time>
+            <span>Diego Fernandes</span>
+          </a>
+
+          <button type="button">
+            Carregar posts
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
 
